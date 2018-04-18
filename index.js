8b3bf2a815a45e97eca38a2d4cff0076d5992b4b
Lamido Tijjani <kinglamido@gmail.com>
2018-04-18 22:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ function nestedTarget(){
 
 function increaseRankBy(n) {
   const lmd = document.querySelectorAll('.ranked-list')
-  const lm = lmd[0]
-  const md = lmd[1]
   for (let i = 0; i < lmd.length; i++){
-    lis[i].innerHTML = (i + 1)
+    lis[i].innerHTML = parseInt((i + n))
 
   }
 }
