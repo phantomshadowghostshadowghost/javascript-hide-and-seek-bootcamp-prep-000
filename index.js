3b86cbd9c5e1ab6e36ce761a37a52998b923e469
Lamido Tijjani <kinglamido@gmail.com>
2018-04-18 22:50:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function increaseRankBy(n) {
   }
 }
 function deepestChild(){
-  const lmd = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const lmd = document.getElementById('grand-node').querySelectorAll('ul.ranked-list li')
 }
