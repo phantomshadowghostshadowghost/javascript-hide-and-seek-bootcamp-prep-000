045395ac23bd5f88e85c659bb984ad2baef78772
Lamido Tijjani <kinglamido@gmail.com>
2018-04-18 22:17:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   const lmd = document.querySelectorAll('.ranked-list')
   const lm = lmd[0]
   const md = lmd[1]
-  for (let i = 0, l = lm.length; i < l; i++){
+  for (let i = 0; i < lmd.length; i++){
     
   }
 }
